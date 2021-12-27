09636fa4e6c22f1440239ebdb76732a2bcfdd73f
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-12-27 17:33:53 +0200

    Aprendi sobre os tipos de dados na linguagem JavaScript, object, string, number, symbol, boolean e undefined.

---
+++ 01-javascript-para-iniciante/tipos-de-dados/script.js
@@ -0,0 +1,49 @@
+var nome = 'Edilson Alexandre'; // string
+console.log(typeof nome);
+
+var idade = 22; // number
+console.log(typeof idade);
+
+var adulto = true; // boolean
+console.log(typeof adulto);
+
+var varUndefined; // undefined
+console.log(typeof varUndefined);
+
+var varNull = null; // null
+console.log(typeof varNull);
+
+var symbol = Symbol(); //symbol
+console.log(typeof symbol);
+
+var object = {}; // object
+var objectNull = null; // object
+console.log(typeof objectNull);
+console.log(typeof object);
+
+// Exercicios
+var varString = `Edilson Alexandre Cuamba`;
+console.log(varString);
+
+
+var varNumeroString = `tenho ${2021-1999} anos de idade` ;
+console.log(varNumeroString);
+
+
+var varIdade = 22;
+console.log(varIdade);
+
+
+var nome = 'Edilson';
+var apelido = 'Cuamba';
+var somaNomes = nome + apelido;
+console.log(somaNomes);
+console.log(`${nome} ${apelido}`);
+
+var fraseExerc = "It's a time.";
+console.log(fraseExerc);
+
+
+
+
+
