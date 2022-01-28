7f5c9dc77d88f21cee8154ae37010e778bc8a069
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-01-28 23:39:36 +0200

    Algun objectos não vamos encontrar em outros host, user client, porque cada um  implementa do seu jeito e por causa das versões alguns ainda nem implementara.

---
+++ 04-objectos/03-native-host-e-user/script.js
@@ -0,0 +1,42 @@
+//Tipode de objecto
+//Existem objectos NAtivos - esses são definidos pela implementação da linguagem e são independentes do navegador em questão
+//Por exemplo tem objectos que não temos no node porque o node  não precisa, como o docuemnt.
+
+// Objectos do user, cada navegador pode ter objectos diferentes.
+
+//Versões diferentes 
+//Browser diferentes podem não ter as mesmas formas de implementação de objectos JS
+
+//Versões antigas de browser novo ou antigo podem não ter implementado certos metodos.
+
+//Tem browsers que não implementaram ECMAScript (ES6) não esta em todos os browsers.
+
+//ECMA organização responsavel por padronizar o JavaScript
+//ECMAScript ES6 - versão do javascript lançada em 2015
+//Engine motor que implementa as especificações JS
+
+//Bibliotecas
+// Conjunto de codigo criado por outros programadores para facilitar o trabalho.
+//JQuery é uma biblioteca ajudava a manter o código equilibrado e fazia funcionar em browsers antigos.
+
+//Verificar se existe, para fazer isso é verificar se o objecto é ou não undefined
+if(typeof Array.from != 'undefined')
+console.log('Existe');
+else
+console.log("não existir");
+
+
+//API - Interface de Aplicativo de programação
+//Interface criada para interagir com outro software.
+//Objectos e metodos do browser são APIs
+
+
+//Exercicios
+//Objectos Nativos String, Array Boolean, Number, 
+
+//Objectos do Host NodeList, HTMLCollection, docuemnt window, history, Location, 
+
+//ONjectos do User webkit (do chrome outros não tem)
+
+
+
