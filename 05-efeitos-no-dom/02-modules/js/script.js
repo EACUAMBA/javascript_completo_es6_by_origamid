1824b3586ba6e61946e99ac5b68dbb5f8a9e716e
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-02-21 21:13:54 +0200

    Aprendi a usar modulos em Javascript para separar melhor o codigo em ficheiros JS.

---
+++ 05-efeitos-no-dom/02-modules/js/script.js
@@ -0,0 +1,9 @@
+import initScrollAnimado from './modules/scroll-animado.js';
+import initScrollSuave from './modules/scroll-suave.js';
+import initTab from './modules/tabnav.js';
+import accordion from './modules/accordion.js';
+
+initScrollSuave();
+initScrollAnimado();
+initTab();
+accordion();
