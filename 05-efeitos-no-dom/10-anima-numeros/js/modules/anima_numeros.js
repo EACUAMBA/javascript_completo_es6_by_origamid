79541ed8bbf35065d6438ae2033fe777d3c2986e
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-03-05 16:44:22 +0200

    Aprendi a fazer conatgem de numeros até o fim, o numero objetivo.

---
+++ 05-efeitos-no-dom/10-anima-numeros/js/modules/anima_numeros.js
@@ -0,0 +1,32 @@
+export default function  initAnimaNumeros(){
+
+    const observerTarget = document.querySelector('.numeros');
+    const numeros = document.querySelectorAll('[data-numero]');
+
+    function anima() {
+        numeros.forEach((numero)=>{
+            const total = +numero.innerHTML;
+            let passo = Math.floor(total / 50);
+            let novoTotal = 0;
+            const timer = setInterval(() => {
+                novoTotal += passo;
+                numero.innerHTML = novoTotal;
+                if(novoTotal > total){
+                    numero.innerHTML = total;
+                    clearInterval(timer)
+                }
+            }, Math.random()*25)
+        })
+    }
+
+    function handleMutation(mutation) {
+        if(mutation[0].target.classList.contains("activo")){
+            observer.disconnect()
+            anima()
+        }
+
+    }
+    const observer = new MutationObserver(handleMutation);
+
+    observer.observe(observerTarget, {attributes: true});
+}
