72b36ac17c1fb570a666bad212566546aebbcebd
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-03-05 09:39:46 +0200

    Aprendi a fazer um menu mobile usando setTimeout para lidar om o event bubbles

---
+++ 05-efeitos-no-dom/09-menu-mobile/js/modules/scroll-animado.js
@@ -0,0 +1,21 @@
+//12 Animação ao Scroll
+export default function animacaoScroll(){
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+  if(!sections.length){
+    return;
+  }
+const windowMetade= window.innerHeight * 0.6;
+function animaScroll(event){
+  sections.forEach((section)=>{
+    const sectionTop = section.getBoundingClientRect().top;
+    let sectionTopMais40Porcento = sectionTop  - windowMetade;
+   if(sectionTopMais40Porcento < 0)
+     section.classList.add("activo")
+   else
+    section.classList.remove("activo")
+   
+  })
+}
+window.onscroll = animaScroll;
+animaScroll();
+}
---
+++ 05-efeitos-no-dom/09-menu-mobile/js/script.js
@@ -0,0 +1,17 @@
+import initScrollAnimado from './modules/scroll-animado.js';
+import initScrollSuave from './modules/scroll-suave.js';
+import initTab from './modules/tabnav.js';
+import accordion from './modules/accordion.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown_menu.js';
+import initMenuMobile from './modules/menu_mobile.js';
+
+initScrollSuave();
+initScrollAnimado();
+initTab();
+accordion();
+initModal()
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
