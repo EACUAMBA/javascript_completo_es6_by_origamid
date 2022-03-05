ab10e753eaa9608224502aa2f272a531cd96dd71
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-03-05 17:13:13 +0200

    Aprendi a trabalhar com datas de forma nativa no JS.

---
+++ 05-efeitos-no-dom/11-date-object/js/modules/funcionamento.js
@@ -0,0 +1,28 @@
+export default function initFuncionamento(){
+
+
+  }
+const funcionamento= document.querySelector("[data-semana]");
+const diasSemana = funcionamento.dataset['semana'].split(', ').map(Number);
+const horarioSemana = funcionamento.dataset['horario'].split(', ').map(Number);
+
+console.log(diasSemana)
+console.log(horarioSemana)
+
+const hoje = new Date();
+const diaAgora = hoje.getDay();
+const horarioAgora = hoje.getHours() - 1;
+
+let horarioAberto = horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1];
+let diaAberto = diasSemana.includes(diaAgora)
+
+if(horarioAberto && diaAberto)
+  funcionamento.classList.add("aberto");
+else
+  funcionamento.classList.add("fechado");
+
+
+
+
+
+
