f60318584a26cbda2e79e46970a3e3a28a83e978
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-03-22 20:16:49 +0200

    Implementei o fetch de bitcoin para mostrar o valor de doação.

---
+++ 06-javascript-assincrono/09-bitcoin-fetch/js/script.js
@@ -0,0 +1,23 @@
+import initScrollAnimado from './modules/scroll-animado.js';
+import initScrollSuave from './modules/scroll-suave.js';
+import initTab from './modules/tabnav.js';
+import accordion from './modules/accordion.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown_menu.js';
+import initMenuMobile from './modules/menu_mobile.js';
+import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
+import initFetchBitcoin from './modules/fetch_bitcoin.js'
+
+initScrollSuave();
+initScrollAnimado();
+initTab();
+accordion();
+initModal()
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initFuncionamento();
+initFetchAnimais();
+initFetchBitcoin()
