8f31237df382ed18ec7e682aca4131a14bf76b43
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-03-22 19:31:31 +0200

    Implementei o fetch de animais.

--- 06-javascript-assincrono/08-animais-fetch/js/modules/fetch-animais.js
+++ 06-javascript-assincrono/08-animais-fetch/js/modules/fetch-animais.js
@@ -1,19 +1,26 @@
+import initAnimaNumeros from "./anima_numeros.js";
 export default function initFetchAnimais(){
+    async function fetchAnimais(URL){
+        const animaisResponse = await fetch(URL);
+        const animaisJson = await animaisResponse.json();
+        const numerosGrid = document.querySelector(".numeros-grid")
+
+        animaisJson.forEach((a)=>{
+            const divAnimal = createAnimal(a)
+            numerosGrid.appendChild(divAnimal);
+        })
+        initAnimaNumeros();
+    }
+
+    function createAnimal( animal){
+        const div = document.createElement("div");
+        div.classList.add("numero-animal")
+        div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.totais}</span>`;
+        return div;
+    }
+
+    fetchAnimais("./animaisapi.json");
 
 }
 
-async function fetchAnimais(URL){
-    const animaisResponse = await fetch(URL);
-    const animaisJson = await animaisResponse.json();
 
-    animaisJson.forEach((a)=>{
-        createAnimal(a)
-    })
-}
-
-function createAnimal( animal){
-    const div = document.createElement("div");
-    div.classList.add("")
-}
-
-fetchAnimais("./animaisapi.json");
--- 06-javascript-assincrono/08-animais-fetch/js/script.js
+++ 06-javascript-assincrono/08-animais-fetch/js/script.js
@@ -6,7 +6,6 @@ import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown_menu.js';
 import initMenuMobile from './modules/menu_mobile.js';
-import initAnimaNumeros from './modules/anima_numeros.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 
@@ -18,6 +17,5 @@ initModal()
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
 initFetchAnimais();
